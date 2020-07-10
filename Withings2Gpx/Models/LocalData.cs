4f606490570711e2db1e029d155e5466c44c8a47
Jan Oratowski <jan.oratowski@gmail.com>
2020-07-10 22:11:58 +0200

    local data

---
+++ Withings2Gpx/Models/LocalData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Withings2Gpx.Models.Withings;
+
+namespace Withings2Gpx.Models
+{
+    class LocalData
+    {
+        public List<HeartRate> HeartRates;
+        public List<Coordinate> Longitudes;
+        public List<Coordinate> Latitudes;
+        public List<Activity> Activities;
+
+        public LocalData()
+        {
+            HeartRates = new List<HeartRate>();
+            Longitudes = new List<Coordinate>();
+            Latitudes = new List<Coordinate>();
+            Activities = new List<Activity>();
+        }
+
+        public static LocalData Load(string file)
+        {
+            if (!File.Exists(file))
+                new LocalData().Save(file);
+            
+            var json = File.ReadAllText(file);
+            return JsonConvert.DeserializeObject<LocalData>(json);
+        }
+
+        public void Save(string file)
+        {
+            var json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(file, json);
+        }
+    }
+}
--- Withings2Gpx/Program.cs
+++ Withings2Gpx/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
@@ -11,6 +12,8 @@ namespace Withings2Gpx
     {
         public static Config Config;
         public static string ConfigPath;
+        public static LocalData Data;
+        public const string LocalDataFile = "data.json";
 
         [STAThread]
         static void Main(string[] args)
@@ -19,20 +22,20 @@ namespace Withings2Gpx
             Config = Config.Load(ConfigPath);
 
             var key = "";
-            var fbd = new FolderBrowserDialog();
-            fbd.SelectedPath = Config.LastPath;
+            var fbd = new FolderBrowserDialog {SelectedPath = Config.LastPath};
 
             if (fbd.ShowDialog() != DialogResult.OK)
                 return;
+
             Config.LastPath = fbd.SelectedPath;
             Config.Save(Config, ConfigPath);
 
+            LoadData(fbd.SelectedPath);
+
             while (key.ToLower() != "q")
             {
-                var activities = new ActivityParser(fbd.SelectedPath).Get().OrderByDescending(a => a.TimeStamp)
-                    .ToList();
                 var i = 0;
-                foreach (var a in activities.Take(50))
+                foreach (var a in Data.Activities.Take(50))
                 {
                     i++;
                     Console.WriteLine($"{i}. {a.TimeStamp} {a.Value}");
@@ -40,9 +43,9 @@ namespace Withings2Gpx
 
                 var command = Console.ReadLine();
 
-                if (activities.Any(a => a.Value.ToLower() == command.ToLower()))
+                if (Data.Activities.Any(a => a.Value.ToLower() == command.ToLower()))
                 {
-                    foreach (var item in activities.Where(a => a.Value.ToLower() == command.ToLower()))
+                    foreach (var item in Data.Activities.Where(a => a.Value.ToLower() == command.ToLower()))
                     {
                         ExportActivity(item, fbd.SelectedPath);
                     }
@@ -50,32 +53,57 @@ namespace Withings2Gpx
                     return;
                 }
 
-                if (!int.TryParse(command, out i) || i > activities.Count)
+                if (!int.TryParse(command, out i) || i > Data.Activities.Count)
                     return;
 
-                var activity = activities[i - 1];
+                var activity = Data.Activities[i - 1];
                 ExportActivity(activity, fbd.SelectedPath);
             }
         }
 
+        private static void LoadData(string path)
+        {
+            var file = Path.Combine(path, LocalDataFile);
+
+            Data = LocalData.Load(file);
+
+            Data.Activities = new ActivityParser(path).Get().OrderByDescending(a => a.TimeStamp)
+                .ToList();
+
+            var heartRates = new HeartRateParser(path).Get();
+            var longitudes = new CoordinateParser(path, CoordinateType.Longitude).Get();
+            var latitudes = new CoordinateParser(path, CoordinateType.Latitude).Get();
+
+            var jsonParser = new JsonParser(path);
+
+            heartRates.AddRange(jsonParser.HeartRates.Where(hr => !heartRates.Select(h => h.TimeStamp).Contains(hr.TimeStamp)));
+            longitudes.AddRange(jsonParser.Longitudes.Where(ln => !longitudes.Select(l => l.TimeStamp).Contains(ln.TimeStamp)));
+            latitudes.AddRange(jsonParser.Latitudes.Where(la => !latitudes.Select(l => l.TimeStamp).Contains(la.TimeStamp)));
+            
+            Data.HeartRates.AddRange(heartRates.Where(hr => !Data.HeartRates.Select(h => h.TimeStamp).Contains(hr.TimeStamp)));
+            Data.Longitudes.AddRange(longitudes.Where(ln => !Data.Longitudes.Select(l => l.TimeStamp).Contains(ln.TimeStamp)));
+            Data.Latitudes.AddRange(latitudes.Where(la => !Data.Latitudes.Select(l => l.TimeStamp).Contains(la.TimeStamp)));
+
+            Data.Save(file);
+        }
 
         private static void ExportActivity(Models.Withings.Activity activity, string path)
         {
             try
             {
-                var heartRates = new HeartRateParser(path).Get(activity.TimeStamp.ToString("yyyy-MM-dd"));
-                var longitudes = new CoordinateParser(path, CoordinateType.Longitude).Get(activity.TimeStamp.ToString("yyyy-MM-dd"));
-                var latitudes = new CoordinateParser(path, CoordinateType.Latitude).Get(activity.TimeStamp.ToString("yyyy-MM-dd"));
+                //var heartRates = new HeartRateParser(path).Get(activity.TimeStamp.ToString("yyyy-MM-dd"));
+                //var longitudes = new CoordinateParser(path, CoordinateType.Longitude).Get(activity.TimeStamp.ToString("yyyy-MM-dd"));
+                //var latitudes = new CoordinateParser(path, CoordinateType.Latitude).Get(activity.TimeStamp.ToString("yyyy-MM-dd"));
 
-                var jsonParser = new JsonParser(path);
+                //var jsonParser = new JsonParser(path);
 
-                heartRates.AddRange(jsonParser.HeartRates.Where(hr => !heartRates.Select(h => h.TimeStamp).Contains(hr.TimeStamp)));
-                longitudes.AddRange(jsonParser.Longitudes.Where(ln => !longitudes.Select(l => l.TimeStamp).Contains(ln.TimeStamp)));
-                latitudes.AddRange(jsonParser.Latitudes.Where(la => !latitudes.Select(l => l.TimeStamp).Contains(la.TimeStamp)));
+                //heartRates.AddRange(jsonParser.HeartRates.Where(hr => !heartRates.Select(h => h.TimeStamp).Contains(hr.TimeStamp)));
+                //longitudes.AddRange(jsonParser.Longitudes.Where(ln => !longitudes.Select(l => l.TimeStamp).Contains(ln.TimeStamp)));
+                //latitudes.AddRange(jsonParser.Latitudes.Where(la => !latitudes.Select(l => l.TimeStamp).Contains(la.TimeStamp)));
 
-                var activityHrs = heartRates.Where(h => h.TimeStamp >= activity.TimeStamp && h.TimeStamp <= activity.End).ToList();
-                var activityLongs = longitudes.Where(l => l.TimeStamp >= activity.TimeStamp && l.TimeStamp <= activity.End).ToList();
-                var activityLats = latitudes.Where(l => l.TimeStamp >= activity.TimeStamp && l.TimeStamp <= activity.End).ToList();
+                var activityHrs = Data.HeartRates.Where(h => h.TimeStamp >= activity.TimeStamp && h.TimeStamp <= activity.End).ToList();
+                var activityLongs = Data.Longitudes.Where(l => l.TimeStamp >= activity.TimeStamp && l.TimeStamp <= activity.End).ToList();
+                var activityLats = Data.Latitudes.Where(l => l.TimeStamp >= activity.TimeStamp && l.TimeStamp <= activity.End).ToList();
 
                 var gpx = new GpxCreator(activity);
 
