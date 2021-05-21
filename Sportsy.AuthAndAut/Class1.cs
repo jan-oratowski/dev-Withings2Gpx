dbcf0006076cfcca5dda1073dccf61ca9d0f0230
Jan Oratowski <54535207+jan-oratowski@users.noreply.github.com>
2021-05-21 21:50:30 +0200

    changes

---
+++ Sportsy.AuthAndAut/Class1.cs
@@ -0,0 +1,22 @@
+﻿using OAuth2.Client.Impl;
+using OAuth2.Infrastructure;
+using System;
+
+namespace Sportsy.AuthAndAut
+{
+    public class Class1
+    {
+        public string GoogleLogin()
+        {
+            var redirectUri = new Uri(Url.Action("GoogleLoginCallBack", "Account", null, protocol: Request.Url.Scheme));
+            var googleClient = new GoogleClient(new RequestFactory(), new OAuth2.Configuration.ClientConfiguration
+            {
+                ClientId = auth.ClientId?.Trim(),
+                ClientSecret = auth.ClientSecret?.Trim(),
+                RedirectUri = redirectUrl,
+                Scope = "profile email"
+            });
+            return googleClient.GetLoginLinkUri("SomeStateValueYouWantToUse");
+        }
+    }
+}
